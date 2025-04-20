e19967fc4894d2601108afbe52c10ad9ddf6b8ea
Mohammad Reza Rajabali <76261792+mrr1368@users.noreply.github.com>
2025-04-20 21:30:17 +0330

    Add Session 02

---
+++ Session 02 DB_Iran.sql
@@ -0,0 +1,40 @@
+﻿/*	=============================================
+
+این اسکریپت یک دیتابیس ساده برای مدل‌سازی ساختار جغرافیایی کشور ایران ایجاد می‌کند.
+دیتابیس شامل سه جدول اصلی برای استان‌ها، شهرستان‌ها و شهرهاست و ارتباط بین آن‌ها به‌صورت
+کلید خارجی تعریف شده. این ساختار می‌تواند به‌عنوان یک پایه تمرینی برای یادگیری روابط 
+بین جدول‌ها در SQL استفاده شود.
+
+=============================================	*/
+
+
+USE master
+GO
+
+IF (SELECT COUNT(*)
+		 FROM sys.databases
+		 WHERE name = 'Session02_DB_Iran' ) = 1
+	 ALTER DATABASE Session02_DB_Iran SET SINGLE_USER 
+	 WITH ROLLBACK IMMEDIATE
+
+DROP DATABASE IF EXISTS Session02_DB_Iran
+GO
+
+
+CREATE DATABASE Session02_DB_Iran
+GO
+
+USE Session02_DB_Iran
+GO
+
+CREATE TABLE Ostan(OstanID int PRIMARY KEY IDENTITY,
+				   OstanName nvarchar(30))
+
+CREATE TABLE Shahrestan(ShahrestanID int PRIMARY KEY IDENTITY,
+					    ShahrestanName nvarchar(30),
+						OstanID int FOREIGN KEY REFERENCES Ostan(OstanID))
+
+
+CREATE TABLE Shahr(ShahrID int PRIMARY KEY IDENTITY,
+				   ShahrName nvarchar(30),
+				   SharestanID int FOREIGN KEY REFERENCES Shahrestan(ShahrestanID))
